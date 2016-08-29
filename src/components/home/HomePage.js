8d26e424f97d43e453f3c4e3017d463706a0a000
Prudhvi Dharmavarapu <Prudhvi Dharmavarapu>
2016-08-30 00:02:34 +0530

    test

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -13,7 +13,7 @@ class HomePage extends React.Component {
         <div className="column paddingright">
             <div className="ui very padded segment heightvh">
             <h1 className="ui center aligned header orange"> PATRON HUB </h1> <br/>
-            <p className="ui center aligned text">t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p className="ui center aligned text">t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose.</p>
             </div>
         </div>
         <div className="column paddingleft"><HomeSlider/></div>
